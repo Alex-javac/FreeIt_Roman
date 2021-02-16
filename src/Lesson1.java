7c636460922a5ec66f1ffa0a6f0d1603eeccaef1
AlexanderAlekseyuk <nix.ya3214@gmail.com>
2021-02-16 09:00:20 +0300

    Added Hello

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Lesson1 {
     public static void main (String[] arg){
-        System.out.println("Hello Java");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ввидите своё имя: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello "+name);
     }
 }
