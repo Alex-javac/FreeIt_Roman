33fa3e3c23bffa1242a28949f00de7d6a60870e3
AlexanderAlekseyuk <nix.ya3214@gmail.com>
2021-03-04 23:45:09 +0300

    add task22 and task23

--- src/lesson3/Task22.java
+++ src/lesson3/Task22.java
@@ -1,9 +1,20 @@
 package lesson3;
+
+import java.math.BigInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 //Написать программу, выполняющую поиск в строке шестнадцатеричных чисел,
 //записаных по правилам Java, с помощью регулярных выражений и вывести их на
 //страницу.
 public class Task22 {
     public static void main(String[] args) {
-
+        String str = "Привет 0xA29F это просто 0x7FFF текст 0x5A3 для проверки 0x7AAF";
+        Pattern hexes = Pattern.compile("0x[A-Fa-f0-9]{1,4}");//"0x\\p{XDigit}+"
+        Matcher hexMatcher = hexes.matcher(str);
+        System.out.println(str);
+        while (hexMatcher.find()){
+            System.out.println(hexMatcher.group());
+        }
     }
 }
