e2d2a66a32d731808e9b440fefc45900316f6da2
AlexanderAlekseyuk <nix.ya3214@gmail.com>
2021-03-06 17:09:30 +0300

    fix logic

--- src/gameXO/GameXO.java
+++ src/gameXO/GameXO.java
@@ -1,5 +1,6 @@
 package gameXO;
 
+import java.util.Random;
 import java.util.Scanner;
 
 //написать игру крестики нолики (двумерный массив размерностью 3х3).
@@ -11,9 +12,11 @@ import java.util.Scanner;
 public class GameXO {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
         boolean playGame = true;
         char[][] field = new char[3][3];
         int count = 0;
+        int limitPatience = 0;
 
         System.out.println("----------Game XO----------");
         System.out.println("Представим поле видом «цифровой клавиатуры»");
@@ -24,116 +27,152 @@ public class GameXO {
         System.out.println("Первым наченает игрок Х");
         System.out.println("-------------GO-------------");
 
-        while (playGame) {
-            char xo = count % 2 == 0 ? 'X' : 'O';//каждый ход миняем знак либо Х либо О
-            System.out.print("Ход игрока " + xo + " : ");
-            int number = scanner.nextInt();
-
-            switch (number) {//подстовляем значение в указанное место и меняем ход игрока
-                case (1):
-                    if (field[2][0] == '\u0000') {
-                        field[2][0] = xo;
-                        count++;
-                    }
-                    break;
-                case (2):
-                    if (field[2][1] == '\u0000') {
-                        field[2][1] = xo;
-                        count++;
-                    }
-                    break;
-                case (3):
-                    if (field[2][2] == '\u0000') {
-                        field[2][2] = xo;
-                        count++;
-                    }
-                    break;
-                case (4):
-                    if (field[1][0] == '\u0000') {
-                        field[1][0] = xo;
-                        count++;
-                    }
-                    break;
-                case (5):
-                    if (field[1][1] == '\u0000') {
-                        field[1][1] = xo;
-                        count++;
-                    }
-                    break;
-                case (6):
-                    if (field[1][2] == '\u0000') {
-                        field[1][2] = xo;
-                        count++;
-                    }
-                    break;
-                case (7):
-                    if (field[0][0] == '\u0000') {
-                        field[0][0] = xo;
-                        count++;
-                    }
-                    break;
-                case (8):
-                    if (field[0][1] == '\u0000') {
-                        field[0][1] = xo;
-                        count++;
-                    }
-                    break;
-                case (9):
-                    if (field[0][2] == '\u0000') {
-                        field[0][2] = xo;
-                        count++;
-                    }
-                    break;
+        do {
+            //предел терпение на ввод некоректных значений (10 раз)
+            if (limitPatience >= 10) {
+                System.out.println("Не можешь срать - не мучай жопу!!!");
+                break;
             }
-            int emptyElements = 9;
-            for (int i = 0; i < field.length; i++) {
-                for (int j = 0; j < field[i].length; j++) {
-                    if (field[i][j] != '\u0000') {//проверяем сколько осталось пустых элементов
-                        emptyElements--;
-
-                    }
-                    System.out.print("[" + field[i][j] + "]");
+            try {
+                char xo = count % 2 == 0 ? 'X' : 'O';//каждый ход миняем знак либо Х либо О
+                System.out.print("Ход игрока " + xo + " : ");
+                int number;
+                if(xo=='O'){
+                    number = random.nextInt(10);
+                }else {
+                    number = Integer.parseInt(scanner.nextLine());
                 }
-                System.out.println();
-            }
+                switch (number) {//подстовляем значение в указанное место и меняем ход игрока
+                    case (1):
+                        if (field[2][0] == '\u0000') {
+                            field[2][0] = xo;
+                            count++;
+                        } else {
+                            System.out.println("Это поле уже занято");
+                        }
+                        break;
+                    case (2):
+                        if (field[2][1] == '\u0000') {
+                            field[2][1] = xo;
+                            count++;
+                        } else {
+                            System.out.println("Это поле уже занято");
+                        }
+                        break;
+                    case (3):
+                        if (field[2][2] == '\u0000') {
+                            field[2][2] = xo;
+                            count++;
+                        } else {
+                            System.out.println("Это поле уже занято");
+                        }
+                        break;
+                    case (4):
+                        if (field[1][0] == '\u0000') {
+                            field[1][0] = xo;
+                            count++;
+                        } else {
+                            System.out.println("Это поле уже занято");
+                        }
+                        break;
+                    case (5):
+                        if (field[1][1] == '\u0000') {
+                            field[1][1] = xo;
+                            count++;
+                        } else {
+                            System.out.println("Это поле уже занято");
+                        }
+                        break;
+                    case (6):
+                        if (field[1][2] == '\u0000') {
+                            field[1][2] = xo;
+                            count++;
+                        } else {
+                            System.out.println("Это поле уже занято");
+                        }
+                        break;
+                    case (7):
+                        if (field[0][0] == '\u0000') {
+                            field[0][0] = xo;
+                            count++;
+                        } else {
+                            System.out.println("Это поле уже занято");
+                        }
+                        break;
+                    case (8):
+                        if (field[0][1] == '\u0000') {
+                            field[0][1] = xo;
+                            count++;
+                        } else {
+                            System.out.println("Это поле уже занято");
+                        }
+                        break;
+                    case (9):
+                        if (field[0][2] == '\u0000') {
+                            field[0][2] = xo;
+                            count++;
+                        } else {
+                            System.out.println("Это поле уже занято");
+                        }
+                        break;
+                    default:
+                        limitPatience++;
+                        System.out.println("Ввидите соответствующее значение от 1 до 9");
+                }
+                System.out.println(limitPatience);
+                int emptyElements = 9;
+                for (char[] chars : field) {
+                    for (char aChar : chars) {
+                        if (aChar != '\u0000') {//проверяем сколько осталось пустых элементов
+                            emptyElements--;
+
+                        }
+                        System.out.print("[" + aChar + "]");
+                    }
+                    System.out.println();
+                }
 
 
-            if (emptyElements == 0) { //Кол-во ходов ограничено
-                System.out.println("Победила дружба!");
-                playGame = false;
-            }
-            //----------проверка по горизонтали
-            if (field[0][0] != '\u0000' && field[0][0] == field[0][1] && field[0][1] == field[0][2]) {
-                System.out.println("Игрок " + xo + " Победил!!!");
-                playGame = false;
-            } else if (field[1][0] != '\u0000' && field[1][0] == field[1][1] && field[1][1] == field[1][2]) {
-                System.out.println("Игрок " + xo + " Победил!!!");
-                playGame = false;
-            } else if (field[2][0] != '\u0000' && field[2][0] == field[2][1] && field[2][1] == field[2][2]) {
-                System.out.println("Игрок " + xo + " Победил!!!");
-                playGame = false;
-            }
-            //---------------проверка по вертикали
-            else if (field[0][0] != '\u0000' && field[0][0] == field[1][0] && field[1][0] == field[2][0]) {
-                System.out.println("Игрок " + xo + " Победил!!!");
-                playGame = false;
-            } else if (field[0][1] != '\u0000' && field[0][1] == field[1][1] && field[1][1] == field[2][1]) {
-                System.out.println("Игрок " + xo + " Победил!!!");
-                playGame = false;
-            } else if (field[0][2] != '\u0000' && field[0][2] == field[1][2] && field[1][2] == field[2][2]) {
-                System.out.println("Игрок " + xo + " Победил!!!");
-                playGame = false;
-            }
-            //---------------проверка по диогонали
-            else if (field[0][0] != '\u0000' && field[0][0] == field[1][1] && field[1][1] == field[2][2]) {
-                System.out.println("Игрок " + xo + " Победил!!!");
-                playGame = false;
-            } else if (field[0][2] != '\u0000' && field[0][2] == field[1][1] && field[1][1] == field[2][0]) {
-                System.out.println("Игрок " + xo + " Победил!!!");
-                playGame = false;
-            }
+                //----------проверка по горизонтали
+                if (field[0][0] != '\u0000' && field[0][0] == field[0][1] && field[0][1] == field[0][2]) {
+                    System.out.println("Игрок " + xo + " Победил!!!");
+                    playGame = false;
+                } else if (field[1][0] != '\u0000' && field[1][0] == field[1][1] && field[1][1] == field[1][2]) {
+                    System.out.println("Игрок " + xo + " Победил!!!");
+                    playGame = false;
+                } else if (field[2][0] != '\u0000' && field[2][0] == field[2][1] && field[2][1] == field[2][2]) {
+                    System.out.println("Игрок " + xo + " Победил!!!");
+                    playGame = false;
+                }
+                //---------------проверка по вертикали
+                else if (field[0][0] != '\u0000' && field[0][0] == field[1][0] && field[1][0] == field[2][0]) {
+                    System.out.println("Игрок " + xo + " Победил!!!");
+                    playGame = false;
+                } else if (field[0][1] != '\u0000' && field[0][1] == field[1][1] && field[1][1] == field[2][1]) {
+                    System.out.println("Игрок " + xo + " Победил!!!");
+                    playGame = false;
+                } else if (field[0][2] != '\u0000' && field[0][2] == field[1][2] && field[1][2] == field[2][2]) {
+                    System.out.println("Игрок " + xo + " Победил!!!");
+                    playGame = false;
+                }
+                //---------------проверка по диогонали
+                else if (field[0][0] != '\u0000' && field[0][0] == field[1][1] && field[1][1] == field[2][2]) {
+                    System.out.println("Игрок " + xo + " Победил!!!");
+                    playGame = false;
+                } else if (field[0][2] != '\u0000' && field[0][2] == field[1][1] && field[1][1] == field[2][0]) {
+                    System.out.println("Игрок " + xo + " Победил!!!");
+                    playGame = false;
+                    //------------------Кол-во ходов ограничено
+                } else if (emptyElements == 0) {
+                    System.out.println("Победила дружба!");
+                    playGame = false;
+                }
 
-        }
+            } catch (NumberFormatException e) {
+                limitPatience++;
+                System.out.println("Please enter a valid number!");
+            }
+        } while (playGame);
         System.out.println("Игра окончена");
     }
 }
