68ba1fff240e4f0e6ac7f396d713e34633cc1bf8
Alexander Alekseyuk <nix.ya3214@gmail.com>
2021-03-16 20:54:16 +0300

    fixed mistakes

--- src/lesson3/Task23.java
+++ src/lesson3/Task23.java
@@ -7,10 +7,12 @@ public class Task23 {
     public static void main(String[] args) {
         String str = "<p id=”p1”> Всё покупается и продаётся, </p>\n" +
                 "<p id=”p15”> И жизнь откровенно над нами смеётся. </p>\n" +
-                "<p id=”p245”> Мы негодуем, мы возмущаемся, </p>\n" +
+                "<div class='green-text'> Мы негодуем, мы возмущаемся, </div>\n" +
                 "<p id=”main”> Но продаёмся и покупаемся. </p>\n" +
                 "<div> Омар Хайям </div>";
         System.out.println(str);
-        System.out.println(str.replaceAll("<p\\s.+?>|</p>|<div>|</div>", "<p>"));
+       String result = str.replaceAll("</div>", "</p>");
+       result = result.replaceAll("<p\\s.+?>|<div>|<div\\s.+?>", "<p>");
+        System.out.println(result);
     }
 }
