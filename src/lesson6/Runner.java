6f1abc52135410e91c1dd773fdaf037d6b81efb2
Alexander Alekseyuk <nix.ya3214@gmail.com>
2021-03-25 23:15:30 +0300

    try fix

--- src/lesson6/Runner.java
+++ src/lesson6/Runner.java
@@ -1,5 +1,53 @@
 package lesson6;
 
-public class Runner {
+import lesson6.annotation.MyService;
+import lesson6.annotation.Version;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+//Написать программу, которая будет анализировать присутствие аннотации над классом MyService
+// и если она присутствует - создавать экземпляр этого класса,
+// задавать значения его полям и вызывать метод thisClassInfo
+// (использовать возможности пакета reflection).
+public class Runner {
+    public static void main(String[] args) {
+        inspectService(MyService.class);
+        inspectService(Object.class);
+
+    }
+
+    static void inspectService(Class<?> service) {
+        try {
+            Class<?> clazz = Class.forName(service.getName());
+            if (service.isAnnotationPresent(Version.class)) {
+                Object obj = clazz.newInstance();//создаем экземпляр класса
+                System.out.println(service.getName());
+
+                Field[] privateField = service.getDeclaredFields(); //массив полей
+                Method[] privateMethod = service.getDeclaredMethods(); // массив методов
+
+                for (Method method : privateMethod) {
+                    int methodModifiers = method.getModifiers();
+                    if (Modifier.isPrivate(methodModifiers)) {//проверяем если метод приватный
+                        System.out.println(method.getName());
+                    }
+                }
+                for (Field fil : privateField) {
+                    int fieldModifiers = fil.getModifiers();
+                    if (Modifier.isPrivate(fieldModifiers)) {//проверяем если поле приватное
+                        System.out.println(fil.getName());
+                    }
+                }
+//         Version ver = service.getAnnotation(Version.class);
+//            System.out.println(ver);
+            } else {
+                System.out.println("Этот класс не содержит анатацию Version");
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException exception) {
+            exception.printStackTrace();
+        }
+    }
 }
--- src/lesson6/annotation/Version.java
+++ src/lesson6/annotation/Version.java
@@ -1,2 +1,15 @@
-package lesson6.annotation;public @interface Version {
+package lesson6.annotation;
+
+//Придумать и написать собственную аннотацию @Version,
+// которая бует хранить информацию о версии класса сервиса (поле обязательное) MyService
+// и кодовое название версии (поле не обязательное) - (например номер версии = 7.1, а кодовое название версии = Nougat).
+
+import java.lang.annotation.*;
+
+@Inherited
+@Target(ElementType.TYPE)
+@Retention(value= RetentionPolicy.RUNTIME)
+public @interface Version {
+double numbVersion();
+String nameVersion();
 }
