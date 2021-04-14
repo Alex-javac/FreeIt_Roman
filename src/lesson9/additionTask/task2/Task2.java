e69aedf4c828624507334b50aab95023b882f615
Alexander Alekseyuk <nix.ya3214@gmail.com>
2021-04-15 01:58:49 +0300

    task2

--- src/lesson9/additionTask/task2/Task2.java
+++ src/lesson9/additionTask/task2/Task2.java
@@ -1,4 +1,8 @@
 package lesson9.additionTask.task2;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
 //Текстовый файл содержит текст. После запуска программы в другой файл
 //должны записаться только те предложения в которых от 3-х до 5-ти слов. Если в
 //предложении присутствует слово-палиндром, то не имеет значения какое кол-во
@@ -16,6 +20,30 @@ package lesson9.additionTask.task2;
 //новый файл
 public class Task2 {
     public static void main(String[] args) {
+        File file = new File("/home/alexander/IdeaProjects/FreeIt_Roman/src/lesson9/additionTask/task2/text.txt");
+        try (
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("/home/alexander/IdeaProjects/FreeIt_Roman/src/lesson9/additionTask/task2/newText.txt"))
+        ) {
+            String str = bufferedReader.readLine();
 
+            while (str != null) {
+                StringTokenizer st = new StringTokenizer(str, "!?.:;«»\"");
+
+                while (st.hasMoreTokens()) {
+                    String sentence = st.nextToken();
+                    int countWords = TextFormater.countWords(sentence);
+
+                    if (TextFormater.isPalindrome(sentence)) {
+                        bufferedWriter.write(sentence + "\n");
+                    } else if (countWords <= 5 && countWords >= 3) {
+                        bufferedWriter.write(sentence + "\n");
+                    }
+                }
+                str = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
